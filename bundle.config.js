97eb95e96caab2445be36c37fc635e56126ce401
amcpanaligan <amcpanaligan@outlook.com>
2016-05-01 08:16:21 +0800

    ys-icebreaker-v2 init

--- bundle.config.js
+++ bundle.config.js
@@ -4,19 +4,24 @@ module.exports = {
             scripts: [
                 './bower_components/jquery/dist/jquery.min.js',
                 './bower_components/Materialize/dist/js/materialize.min.js',
+                './bower_components/firebase/firebase.js',
                 './bower_components/angular/angular.min.js',
                 './bower_components/angular-aria/angular-aria.min.js',
                 './bower_components/angular-animate/angular-animate.min.js',
                 './bower_components/angular-material/angular-material.min.js',
                 './bower_components/angular-sanitize/angular-sanitize.min.js',
+                './bower_components/angularfire/dist/angularfire.min.js',
                 './bower_components/a0-angular-storage/dist/angular-storage.min.js',
-                './bower_components/angular-ui-router/release/angular-ui-router.min.js'
+                './bower_components/angular-ui-router/release/angular-ui-router.min.js',
+                './bower_components/angular-google-plus/dist/angular-google-plus.min.js'
             ],
             styles: [
                 './bower_components/Materialize/dist/css/materialize.min.css',
                 './bower_components/angular-material/angular-material.min.css'
-            ]
+            ],
+            options: {
+                rev: false
+            }
         }
-    },
-    //copy: './content/**/*.{png,svg}'
+    }
 };
